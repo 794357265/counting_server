3a2332d05c55c7157c49849731794c1ec9fdfa3e
zyb <794357265@qq.com>
2019-07-22 18:01:28 +0800

    20190722:baseController

--- counting_server/src/main/java/com/zyb/controller/BaseController.java
+++ counting_server/src/main/java/com/zyb/controller/BaseController.java
@@ -1,10 +1,26 @@
 package com.zyb.controller;
 
+import com.zyb.error.BusinessException;
+import com.zyb.error.EmBusinessError;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin
 public class BaseController {
 
     public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
+
+    @ExceptionHandler({BusinessException.class})
+    @ResponseBody
+    public EmBusinessError handlerException(BusinessException e) {
+        System.out.println(e.getErrMsg());
+        e.printStackTrace();
+        EmBusinessError emBusinessError = EmBusinessError.UNKNOWN_ERROR;
+        emBusinessError.setErrMsg(e.getErrMsg());
+        return emBusinessError;
+    }
 
 }
--- counting_server/src/main/java/com/zyb/controller/ItemController.java
+++ counting_server/src/main/java/com/zyb/controller/ItemController.java
@@ -1,10 +1,14 @@
 package com.zyb.controller;
 
+import com.zyb.error.BusinessException;
+import com.zyb.error.EmBusinessError;
 import com.zyb.response.CommonReturnType;
 import com.zyb.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,23 +20,29 @@ import javax.servlet.http.HttpServletResponse;
  *
  */
 @Controller
+@RequestMapping("/item")
+@CrossOrigin
 public class ItemController extends BaseController{
 	
 	@Autowired
 	private ItemService itemService;
 
-	@RequestMapping("/queryItem")
+	@RequestMapping(value="/queryItem",consumes={CONTENT_TYPE_FORMED},method={RequestMethod.POST})
 	@ResponseBody
-	public CommonReturnType queryItem(HttpServletRequest request,HttpServletResponse response) {
+	public CommonReturnType queryItem(HttpServletRequest request,HttpServletResponse response) throws BusinessException {
 		System.out.println("queryItem!");
         String name = request.getParameter("name");
-        System.out.println("queryItem!-->>"+name);
+        if(name == null || "".equals(name)){
+			throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
+		}
+		System.out.println("queryItem!-->>"+name);
         return CommonReturnType.create("queryItem-->>");
 	}
 
 	@RequestMapping("/queryItemById")
-	public void queryItemById(HttpServletRequest request,HttpServletResponse response) throws Exception{
-
+	@ResponseBody
+	public String queryItemById(HttpServletRequest request,HttpServletResponse response) throws Exception{
+		return "{'queryItemById':'2'}";
 	}
 	
 	@RequestMapping("/deleteItemById")
